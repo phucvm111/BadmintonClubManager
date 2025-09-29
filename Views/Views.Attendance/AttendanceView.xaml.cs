8966d6bb74c429b798a4ddf63bb3f630e38182ac
phucvm111 <phucthcsvt@gmail.com>
2025-09-29 14:46:31 +0700

    add AttendanceView + MemberViews

---
+++ Views/Views.Attendance/AttendanceView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Controls;
+using BadmintonClub.ViewModels;
+
+namespace BadmintonClub.Views;
+
+public partial class AttendanceView : UserControl
+{
+    public AttendanceView()
+    {
+        InitializeComponent();
+        Loaded += AttendanceView_Loaded;
+    }
+
+    private void AttendanceView_Loaded(object sender, System.Windows.RoutedEventArgs e)
+    {
+        if (DataContext is AttendanceViewModel vm)
+        {
+            vm.CommitEditRequested = () =>
+            {
+                AttGrid.CommitEdit(DataGridEditingUnit.Cell, true);
+                AttGrid.CommitEdit(DataGridEditingUnit.Row, true);
+            };
+        }
+    }
+}
---
+++ Views/Views.Members/MembersView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+using BadmintonClub.ViewModels;
+
+namespace BadmintonClub.Views;
+
+public partial class MembersView : UserControl
+{
+    public MembersView()
+    {
+        InitializeComponent();
+        Loaded += MembersView_Loaded;
+    }
+
+    private void MembersView_Loaded(object sender, System.Windows.RoutedEventArgs e)
+    {
+        if (DataContext is MembersViewModel vm)
+        {
+            // Cho ViewModel yêu cầu commit edit trước khi Save/Delete
+            vm.CommitEditRequested = () =>
+            {
+                MembersGrid.CommitEdit(DataGridEditingUnit.Cell, true);
+                MembersGrid.CommitEdit(DataGridEditingUnit.Row, true);
+            };
+        }
+    }
+}
